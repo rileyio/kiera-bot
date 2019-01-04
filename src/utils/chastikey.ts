9b6125cafb87fbf50069c7bb40260aeddda522d1
Emma Booker <riley@tdm.io>
2019-01-04 11:31:25 -0400

    Adding in `ts` for ChastiKey ticker retrieval due to image caching

--- src/utils/chastikey.ts
+++ src/utils/chastikey.ts
@@ -4,7 +4,12 @@ export namespace ChastiKey {
   export function generateTickerURL(ck: TrackedChastiKey) {
     const date = `${ck.ticker.startDateYY || ''}-${ck.ticker.startDateDD || ''}-${ck.ticker.startDateMM || ''}`
     const tickerType = ck.ticker.type
+    // Break url into parts for easier building
+    const fd = `fd=${date}`
+    const un = `un=${ck.username}`
+    const ts = `ts=${Date.now()}`
+    const ext = `ext=.png`
 
-    return `http://www.chastikey.com/tickers/ticker.php?ty=${tickerType}&st=1&un=${ck.username}&fd=${date}&r=0&ext=.png`
+    return `http://www.chastikey.com/tickers/ticker.php?ty=${tickerType}&${ts}&${un}&${fd}&r=0&${ext}`
   }
 }
