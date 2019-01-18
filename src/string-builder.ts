7b183cd55181629917e6fcac22f945afea262fe3
Emma Booker <riley@tdm.io>
2019-01-18 08:56:10 -0400

    Utils.sb (StringBuilder) now has defaults if nothing is passed for prefix

--- src/string-builder.ts
+++ src/string-builder.ts
@@ -1,10 +1,16 @@
 export function sb(baseString: string, data?: any) {
+  // Defaults to use if not passed in data
+  var globals = {
+    prefix: process.env.BOT_MESSAGE_PREFIX
+  }
+
+  // Merge data with global defaults
+  Object.assign(globals, data || {})
+
   var final = baseString || ``
 
-  if (data !== undefined) {
-    for (const key in data) {
-      final = final.replace(new RegExp(`{{${key}}}`, 'img'), data[key])
-    }
+  for (const key in globals) {
+    final = final.replace(new RegExp(`{{${key}}}`, 'img'), globals[key])
   }
 
   return final
