09a5d62ce9a7b73d79e819d61ff1b2662bb5a780
Emma Booker <emma@tdm.io>
2019-02-19 22:14:18 -0400

    correcting commented file that should not have been

--- src/api/controllers/lists.ts
+++ src/api/controllers/lists.ts
@@ -1,35 +1,35 @@
-// import * as Validation from '../validations/index';
-// import * as errors from 'restify-errors';
-// import { validate } from '../utils/validate';
-// import { WebRouted } from '../web-router';
+import * as Validation from '../validations/index';
+import * as errors from 'restify-errors';
+import { validate } from '../utils/validate';
+import { WebRouted } from '../web-router';
 
-// export namespace Lists {
-//   export async function get(routed: WebRouted) {
-//     const v = await validate(Validation.Lists.get(), routed.req.body)
-//     const payload = {
-//       users: [],
-//       servers: []
-//     }
+export namespace Lists {
+  export async function get(routed: WebRouted) {
+    const v = await validate(Validation.Lists.get(), routed.req.body)
+    const payload = {
+      users: [],
+      servers: []
+    }
 
-//     // this.DEBUG_WEBAPI('req params', v.o)
+    // this.DEBUG_WEBAPI('req params', v.o)
 
-//     if (v.valid) {
-//       var users = await routed.Bot.DB.getMultiple('users', {
-//         username: { $regex: new RegExp(`^${v.o.input}`), $options: 'i' }
-//       }, { username: 1, discriminator: 1 })
-//       var servers = await routed.Bot.DB.getMultiple('servers', {
-//         name: { $regex: new RegExp(`^${v.o.input}`), $options: 'i' }
-//       }, { name: 1, region: 1, ownerID: 1 })
+    if (v.valid) {
+      var users = await routed.Bot.DB.getMultiple('users', {
+        username: { $regex: new RegExp(`^${v.o.input}`), $options: 'i' }
+      }, { username: 1, discriminator: 1 })
+      var servers = await routed.Bot.DB.getMultiple('servers', {
+        name: { $regex: new RegExp(`^${v.o.input}`), $options: 'i' }
+      }, { name: 1, region: 1, ownerID: 1 })
 
-//       payload.servers = servers
-//       payload.users = users
+      payload.servers = servers
+      payload.users = users
 
-//       return routed.res.send(payload);
-//     }
+      return routed.res.send(payload);
+    }
 
-//     // On error
-//     return routed.next(new errors.BadRequestError());
-//   }
+    // On error
+    return routed.next(new errors.BadRequestError());
+  }
 
 
-// }
+}
