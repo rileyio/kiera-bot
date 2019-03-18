f814c520c9dac253794863928b1e692aa6f26fa7
Emma Booker <emma@tdm.io>
2019-03-18 07:36:38 -0300

    Fixed: OAuth issue where users who have no avatar set were getting blocked by validation

--- src/api/validations/user.ts
+++ src/api/validations/user.ts
@@ -21,7 +21,7 @@ export namespace User {
       premium_type: joi.number().optional(),
       mfa_enabled: joi.boolean().required(),
       flags: joi.number().required(),
-      avatar: joi.string().alphanum().min(32).max(32).required(),
+      avatar: joi.string().alphanum().min(32).max(32).allow(null).required(),
       discriminator: joi.string().alphanum().min(4).max(4).required(),
       id: joi.string().alphanum().min(2).max(24).required(),
       provider: joi.string().required(),
