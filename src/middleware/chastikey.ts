211618644d527fab1ae15291b34491279908c717
Emma Booker <emma@tdm.io>
2019-10-03 23:44:32 -0300

    Updated: CK isCKVerified middleware to check new dataset, will speed up step.

--- src/middleware/chastikey.ts
+++ src/middleware/chastikey.ts
@@ -1,9 +1,8 @@
 import { RouterRouted } from '../router/router';
 
 export async function isCKVerified(routed: RouterRouted) {
-  const isVerifiedLockee = await routed.bot.DB.verify('ck-lockees', { discordID: Number(routed.user.id) })
-  const isVerifiedKH = await routed.bot.DB.verify('ck-keyholders', { discordID: Number(routed.user.id) })
-  if (isVerifiedLockee || isVerifiedKH) return routed // No need to hault if this passes
+  const isVerified = await routed.bot.DB.verify('ck-users', { discordID: Number(routed.user.id) })
+  if (isVerified) return routed // No need to hault if this passes
   // Fallback, user not yet registered
   await routed.message.reply(
     'This command requires your account be verified with ChastiKey using the following command: `!ck verify`'
