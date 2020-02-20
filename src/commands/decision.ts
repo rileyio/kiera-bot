622dfac90c546fa0c896e4a5f3e49d4bbf3e9588
RileyIO <riley@tdm.io>
2020-02-20 01:05:48 -0400

    Added: Decision Server Whitelisting commands and implemented at roll runtime (Fails with a message).

--- src/commands/decision.ts
+++ src/commands/decision.ts
@@ -67,7 +67,7 @@ export const Routes = ExportRoutes(
     category: 'Fun',
     commandTarget: 'author',
     controller: unblacklistUser,
-    example: '{{prefix}}decision "id" unblacklist user 146439529824256000',
+    example: '{{prefix}}decision "id" unblacklist user "userSnowflake"',
     name: 'decision-unblacklist-user',
     validate: '/decision:string/id=string/unblacklist:string/user:string/user=string',
     middleware: [Middleware.isUserRegistered]
@@ -77,7 +77,7 @@ export const Routes = ExportRoutes(
     category: 'Fun',
     commandTarget: 'author',
     controller: blacklistUser,
-    example: '{{prefix}}decision "id" blacklist user 146439529824256000',
+    example: '{{prefix}}decision "id" blacklist user "userSnowflake"',
     name: 'decision-blacklist-user',
     validate: '/decision:string/id=string/blacklist:string/user:string/user=string',
     middleware: [Middleware.isUserRegistered]
@@ -90,6 +90,26 @@ export const Routes = ExportRoutes(
     example: '{{prefix}}decision "id" blacklisted users',
     name: 'decision-blacklist-show',
     validate: '/decision:string/id=string/blacklisted:string/users:string',
+    middleware: [Middleware.isUserRegistered]
+  },
+  {
+    type: 'message',
+    category: 'Fun',
+    commandTarget: 'author',
+    controller: unwhitelistServer,
+    example: '{{prefix}}decision "id" unwhitelist server "serverID"',
+    name: 'decision-whitelist-server',
+    validate: '/decision:string/id=string/unwhitelist:string/server:string/serverid=string',
+    middleware: [Middleware.isUserRegistered]
+  },
+  {
+    type: 'message',
+    category: 'Fun',
+    commandTarget: 'author',
+    controller: whitelistServer,
+    example: '{{prefix}}decision "id" whitelist server "serverID"',
+    name: 'decision-whitelist-server',
+    validate: '/decision:string/id=string/whitelist:string/server:string/serverid=string',
     middleware: [Middleware.isUserRegistered]
   },
   {
@@ -155,13 +175,21 @@ export async function runSavedDecision(routed: RouterRouted) {
 
     // Halt if user blacklist is triggered
     if (decision.userBlacklist.findIndex(u => u === routed.user.id) > -1) {
-      return true
+      return true // Stop here
+    }
+
+    // Halt if decision rolled on server is not whitelisted
+    if (decision.serverWhitelist.length > 0) {
+      if (decision.serverWhitelist.findIndex(s => s === routed.message.guild.id) === -1) {
+        await routed.message.reply(`This decision roll (\`${decision._id.toString()}\`) cannot be used on this server!`)
+        return true // Stop here
+      }
     }
 
     // Halt if decision is disabled
     if (decision.enabled === false) {
       await routed.message.reply(`Decision not enabled!`)
-      return true
+      return true // Stop here
     }
 
     // Lookup author
@@ -271,7 +299,7 @@ export async function blacklistUser(routed: RouterRouted) {
       .then(u => {
         return u
       })
-      .catch(u => {
+      .catch(e => {
         return null
       })
     const userExists = !!user
@@ -303,6 +331,8 @@ export async function blacklistUser(routed: RouterRouted) {
       // Inform user
       await routed.message.reply(`User \`${user.id}\` was already blacklisted.`)
     }
+
+    return true
   }
 
   return false
@@ -318,7 +348,7 @@ export async function unblacklistUser(routed: RouterRouted) {
       .then(u => {
         return u
       })
-      .catch(u => {
+      .catch(e => {
         return null
       })
     const userExists = !!user
@@ -351,6 +381,8 @@ export async function unblacklistUser(routed: RouterRouted) {
       await routed.bot.DB.update('decision', { _id: new ObjectID(routed.v.o.id) }, { $set: { userBlacklist: decision.userBlacklist } }, { atomic: true })
       await routed.message.reply(`User \`${user.id}\` is no longer blacklisted on this decision roller.`)
     }
+
+    return true
   }
 
   return false
@@ -389,6 +421,69 @@ export async function showUserBlacklist(routed: RouterRouted) {
 
     // DM list to caller
     await routed.message.author.send(response)
+
+    return true
+  }
+
+  return false
+}
+
+export async function whitelistServer(routed: RouterRouted) {
+  const decisionFromDB = await routed.bot.DB.get<TrackedDecision>('decision', { _id: new ObjectID(routed.v.o.id), authorID: routed.user.id })
+
+  if (decisionFromDB) {
+    // Construct
+    const decision = new TrackedDecision(decisionFromDB)
+
+    // Check if server id is already in the whitelist
+    const isAlreadyWhitelisted = decision.serverWhitelist.findIndex(s => s === routed.v.o.serverid) > -1
+
+    // When server id is NOT whitelisted add it
+    if (!isAlreadyWhitelisted) {
+      decision.serverWhitelist.push(routed.v.o.serverid)
+
+      // Update DB
+      await routed.bot.DB.update('decision', { _id: new ObjectID(routed.v.o.id) }, { $set: { serverWhitelist: decision.serverWhitelist } }, { atomic: true })
+
+      // Inform user
+      await routed.message.reply(`Server \`${routed.v.o.serverid}\` added to server whitelist.`)
+    } else {
+      // Inform user
+      await routed.message.reply(`Server \`${routed.v.o.serverid}\` was already whitelisted.`)
+    }
+
+    return true
+  }
+
+  return false
+}
+
+export async function unwhitelistServer(routed: RouterRouted) {
+  const decisionFromDB = await routed.bot.DB.get<TrackedDecision>('decision', { _id: new ObjectID(routed.v.o.id), authorID: routed.user.id })
+
+  if (decisionFromDB) {
+    // Construct
+    const decision = new TrackedDecision(decisionFromDB)
+
+    // Check if server id is already in the whitelist
+    const isAlreadyWhitelisted = decision.serverWhitelist.findIndex(s => s === routed.v.o.serverid) > -1
+
+    // When server id is NOT whitelisted add it
+    if (!isAlreadyWhitelisted) {
+      await routed.message.reply(`Server \`${routed.v.o.serverid}\` is not whitelisted on this decision roller.`)
+    } else {
+      // Remove user from blacklist
+      decision.serverWhitelist.splice(
+        decision.serverWhitelist.findIndex(s => s === routed.v.o.serverid),
+        1
+      )
+
+      // Update DB
+      await routed.bot.DB.update('decision', { _id: new ObjectID(routed.v.o.id) }, { $set: { serverWhitelist: decision.serverWhitelist } }, { atomic: true })
+      await routed.message.reply(`User \`${routed.v.o.serverid}\` is no longer whitelisted on this decision roller.`)
+    }
+
+    return true
   }
 
   return false
