fed2b30b772b0228397d180648dbaf877e8a3f75
Emma (RileyIO) <riley@tdm.io>
2020-04-05 22:06:22 -0300

    Update to specify route files search depth

--- src/api/router/route-loader.ts
+++ src/api/router/route-loader.ts
@@ -4,19 +4,19 @@ import { WebRoute } from '@/api/web-router'
 
 export function webRouteLoader() {
   // Load routes from commands folder
-  const _routeFiles = glob.sync(['app/api/controllers/**/*.js', '!app/api/controllers/**/index.js'], { deep: true })
+  const _routeFiles = glob.sync(['app/api/controllers/**/*.js', '!app/api/controllers/**/index.js'], { deep: 5 })
 
   // Collection of routes
   var routes: Array<WebRoute> = []
 
   // Load each route configured into the routes array
-  _routeFiles.forEach(routeFile => {
+  _routeFiles.forEach((routeFile) => {
     // Wrapped in a try to make more safe when loading and errors are present
     try {
       const _requiredFile: { Routes: Array<WebRoute> } = require(Path.join('../../../', routeFile.toString()))
       // Test if file returns undefined
       if (_requiredFile !== undefined) {
-        console.log(`webRouteLoader() => ${routeFile.toString()}, ${_requiredFile.Routes.map(r => Array.isArray(r)).length}`)
+        console.log(`webRouteLoader() => ${routeFile.toString()}, ${_requiredFile.Routes.map((r) => Array.isArray(r)).length}`)
 
         for (let index = 0; index < _requiredFile.Routes.length; index++) {
           const route = _requiredFile.Routes[index]
