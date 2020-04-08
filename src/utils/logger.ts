62b056ee9fbca53b6c210ccd8b7e45b7d42f07d1
Emma (RileyIO) <riley@tdm.io>
2020-04-07 22:30:59 -0300

    Updating Winston variable & import alias

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,16 +1,15 @@
 import * as _Debug from 'debug'
-import * as _Winston from 'winston'
+import * as Winston from 'winston'
 import { Types } from '@/utils'
 
 export namespace Logging {
   export class Debug {
     public readonly name: string
-    private _debug: _Debug.IDebugger
-    private _winston: _Winston.Logger
-    private fileFormat = _Winston.format.printf(({ level, message, label, timestamp }) => {
+    private winston: Winston.Logger
+    private fileFormat = Winston.format.printf(({ level, message, label, timestamp }) => {
       return `${timestamp} ${level} ${message}`
     })
-    private consoleFormat = _Winston.format.printf(({ level, message, label, timestamp }) => {
+    private consoleFormat = Winston.format.printf(({ level, message, label, timestamp }) => {
       return `${message}`
     })
 
@@ -31,40 +30,35 @@ export namespace Logging {
 
       var loggerProps = {
         level: process.env.BOT_LOGGING || 'debug',
-        transports: [
-          new _Winston.transports.File({ filename: `./logs/${this.name}.log` }),
-          new _Winston.transports.Console({ format: _Winston.format.combine(this.consoleFormat) })
-        ],
-        format: _Winston.format.combine(_Winston.format.timestamp(), this.fileFormat)
+        transports: [new Winston.transports.File({ filename: `./logs/${this.name}.log` }), new Winston.transports.Console({ format: Winston.format.combine(this.consoleFormat) })],
+        format: Winston.format.combine(Winston.format.timestamp(), this.fileFormat)
       }
 
       // If console.opts is set to false, don't add it
-      if (!this.options.console || !this._toConsole) loggerProps.transports = [new _Winston.transports.File({ filename: `./logs/${this.name}.log` })]
-
-      // this._debug = _Debug(name)
-      this._winston = _Winston.createLogger(loggerProps)
+      if (!this.options.console || !this._toConsole) loggerProps.transports = [new Winston.transports.File({ filename: `./logs/${this.name}.log` })]
+      this.winston = Winston.createLogger(loggerProps)
 
       // Keep Winston from an exit if it fails
-      this._winston.exitOnError = false
+      this.winston.exitOnError = false
 
       // Bind _debug.log to console
       // this._debug.log = console.log.bind(console);
     }
 
     public log(...args: Array<any>) {
-      this._winston.info(args.map((a) => (typeof a === 'object' ? JSON.stringify(a) : a)).join(' '))
+      this.winston.info(args.map((a) => (typeof a === 'object' ? JSON.stringify(a) : a)).join(' '))
     }
 
     public warn(...args: Array<any>) {
-      this._winston.warn(args.map((a) => (typeof a === 'object' ? JSON.stringify(a) : a)).join(' '))
+      this.winston.warn(args.map((a) => (typeof a === 'object' ? JSON.stringify(a) : a)).join(' '))
     }
 
     public error(...args: Array<any>) {
-      this._winston.error(args.map((a) => (typeof a === 'object' ? JSON.stringify(a) : a)).join(' '))
+      this.winston.error(args.map((a) => (typeof a === 'object' ? JSON.stringify(a) : a)).join(' '))
     }
 
     public debug(...args: Array<any>) {
-      this._winston.debug(args.map((a) => (typeof a === 'object' ? JSON.stringify(a) : a)).join(' '))
+      this.winston.debug(args.map((a) => (typeof a === 'object' ? JSON.stringify(a) : a)).join(' '))
     }
   }
 }
