b4ab6a5391f4b8b2b261ad1be621f4021d0c2ab7
Emma (RileyIO) <riley@tdm.io>
2020-05-16 19:11:33 -0300

    Changed fetchUsersCount helper to calc from guilds cache size

--- src/utils/stats.ts
+++ src/utils/stats.ts
@@ -4,7 +4,10 @@ export async function fetchUserCounts(Bot: Bot) {
   var totalUsers = 0
   var totalRegistered = 0
 
-  totalUsers = Bot.client.users.cache.size
+  totalUsers = Bot.client.guilds.cache
+    .array()
+    .map((g) => g.memberCount)
+    .reduce((prev, cur) => (cur += prev))
 
   // Get total registered to the bot
   totalRegistered = await Bot.DB.count('users', {})
