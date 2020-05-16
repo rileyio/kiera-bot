3855ab38d1b6d9eaf185253eec2f102be3fbc20e
Emma (RileyIO) <riley@tdm.io>
2020-05-16 15:18:28 -0300

    Setting ecosystem to monitor locales

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // node_args: "DEBUG='kiera-bot:*'",
     instances: 1,
     autorestart: true,
-    watch: ['app'],
+    watch: ['app', 'locales'],
     ignore_watch: ['node_modules', 'logs'],
     max_memory_restart: '1G',
     env: {
