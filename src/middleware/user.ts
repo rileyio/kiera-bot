462d18673f368b8aa52d6663c04a207d0ed7cc2e
Emma RileyIO <riley@tdm.io>
2020-05-31 15:01:06 -0300

    Updating en locale key UserNotRegistered

--- src/middleware/user.ts
+++ src/middleware/user.ts
@@ -4,11 +4,7 @@ export async function isUserRegistered(routed: RouterRouted) {
   const isRegistered = await routed.bot.DB.verify('users', routed.user.id)
   if (isRegistered) return routed // No need to hault if this passes
   // Fallback, user not yet registered
-  await routed.message.reply(
-    'This command requires you to be registered, this can be done using: `!register`\n\
-     Registering opts you into having some basic account user ids stored in order for the\n\
-     bot to function normally for commands requiring stored values.'
-  )
+  await routed.message.reply(routed.$render('Generic.Info.UserNotRegistered'))
   return // Returns nothing which halts going any further
 }
 
