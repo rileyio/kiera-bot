fc50fae613fb3a69cbbaeb69c334a714c9545fe6
Emma RileyIO <riley@tdm.io>
2020-06-11 20:43:56 -0300

    CK Command Middleware update to Record Update in db

--- src/middleware/chastikey.ts
+++ src/middleware/chastikey.ts
@@ -1,24 +1,23 @@
-import * as Utils from '@/utils'
 import { RouterRouted } from '@/router'
-import { TrackedUser } from '@/objects/user'
 import { UserData } from 'chastikey.js/app/objects'
 
 export async function isCKVerified(routed: RouterRouted) {
   const ckUser = new UserData(
     await routed.bot.DB.get<UserData>('ck-users', { discordID: routed.author.id })
   )
-  // Check kiera's as well, override where special conditions apply
-  const kieraUser = new TrackedUser(
-    await routed.bot.DB.get<TrackedUser>('users', { $or: [{ id: routed.author.id }, { 'ChastiKey.username': new RegExp(`^${ckUser.username}$`, 'i') }] })
-  )
 
   // Update kiera's record if something is missing
-  if ((!ckUser.userID && kieraUser._id) || ckUser.username !== kieraUser.ChastiKey.username) {
-    await routed.bot.DB.update('users', { id: routed.author.id }, { $set: { 'ChastiKey.isVerified': ckUser.isVerified, 'ChastiKey.username': ckUser.username } }, { atomic: true })
+  if ((ckUser.username !== routed.user.ChastiKey.username || !routed.user.ChastiKey.isVerified) && ckUser.username !== '') {
+    await routed.bot.DB.update(
+      'users',
+      { id: routed.author.id },
+      { $set: { 'ChastiKey.isVerified': !routed.user.ChastiKey.isVerified ? true : false, 'ChastiKey.username': ckUser.username } },
+      { atomic: true }
+    )
   }
 
   // Verified!!
-  if (ckUser.userID !== undefined || kieraUser.ChastiKey.isVerified) return routed // No need to hault if this passes
+  if (ckUser.userID !== undefined || routed.user.ChastiKey.isVerified) return routed // No need to hault if this passes
   // Fallback, user not yet registered
   await routed.message.reply(routed.$render('ChastiKey.Verify.VerifyRequired'))
   return // Returns nothing which halts going any further
