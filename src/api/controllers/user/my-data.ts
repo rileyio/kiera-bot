06fd4f9adb84bc47d4a6c9c439b10a56be2abbf2
Emma RileyIO <riley@tdm.io>
2020-06-16 23:42:48 -0300

    Improving the API MyDataTotals for future scaling

--- src/api/controllers/user/my-data.ts
+++ src/api/controllers/user/my-data.ts
@@ -12,32 +12,26 @@ export const Routes: Array<WebRoute> = [
 ]
 
 export async function getMyDataTotals(routed: WebRouted) {
-  const totals = {
-    kiera: {
-      auditLog: await routed.Bot.DB.count('audit-log', { owner: routed.session.userID }),
-      botSettings: await routed.Bot.DB.count('settings', { author: routed.session.userID }),
-      commandPermissions: await routed.Bot.DB.count('command-permissions', { userID: routed.session.userID }),
-      decision: await routed.Bot.DB.count('decision', { authorID: routed.session.userID }),
-      decisionLog: await routed.Bot.DB.count('decision-log', { callerID: routed.session.userID }),
-      messages: await routed.Bot.DB.count('messages', { callerID: routed.session.userID }),
-      mutedUsersAsUser: await routed.Bot.DB.count('muted-users', { id: routed.session.userID }),
-      mutedUsersAsMuter: await routed.Bot.DB.count('muted-users', { mutedById: routed.session.userID }),
-      polls: await routed.Bot.DB.count('polls', { authorID: routed.session.userID }),
-      serverSettings: await routed.Bot.DB.count('server-settings', { authorID: routed.session.userID }),
-      serversAsOwner: await routed.Bot.DB.count('servers', { owner: routed.session.userID }),
-      sessions: await routed.Bot.DB.count('sessions', { userID: routed.session.userID }),
-      stats: await routed.Bot.DB.count('stats-servers', { userID: routed.session.userID }),
-      statsSettings: await routed.Bot.DB.count('stats-settings', { userID: routed.session.userID }),
-      users: await routed.Bot.DB.count('users', { id: routed.session.userID })
-    },
-    thirdParty: {
-      chastikey: {
-        locktober: await routed.Bot.DB.count('ck-locktober', { discordID: routed.session.userID }),
-        runningLocks: await routed.Bot.DB.count('ck-running-locks', { discordID: routed.session.userID }),
-        users: await routed.Bot.DB.count('ck-users', { discordID: routed.session.userID })
-      }
-    }
-  }
+  var totals = [
+    { thirdParty: false, scope: 'kiera', name: 'Audit Log', count: await routed.Bot.DB.count('audit-log', { owner: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Bot Settings', count: await routed.Bot.DB.count('settings', { author: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Command Permissions', count: await routed.Bot.DB.count('command-permissions', { userID: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Decisions', count: await routed.Bot.DB.count('decision', { authorID: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Decision Log', count: await routed.Bot.DB.count('decision-log', { callerID: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Message', count: await routed.Bot.DB.count('messages', { callerID: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Muted as User', count: await routed.Bot.DB.count('muted-users', { id: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Muted as Muter', count: await routed.Bot.DB.count('muted-users', { mutedById: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Polls', count: await routed.Bot.DB.count('polls', { authorID: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Server Settings', count: await routed.Bot.DB.count('server-settings', { authorID: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Servers as Owner', count: await routed.Bot.DB.count('servers', { owner: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Web Sessions', count: await routed.Bot.DB.count('sessions', { userID: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Stats', count: await routed.Bot.DB.count('stats-servers', { userID: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Stats Settings', count: await routed.Bot.DB.count('stats-settings', { userID: routed.session.userID }) },
+    { thirdParty: false, scope: 'kiera', name: 'Users', count: await routed.Bot.DB.count('users', { id: routed.session.userID }) },
+    { thirdParty: true, scope: 'chastikey', name: 'Locktober', count: await routed.Bot.DB.count('ck-locktober', { discordID: routed.session.userID }) },
+    { thirdParty: true, scope: 'chastikey', name: 'Running Locks', count: await routed.Bot.DB.count('ck-running-locks', { discordID: routed.session.userID }) },
+    { thirdParty: true, scope: 'chastikey', name: 'Users', count: await routed.Bot.DB.count('ck-users', { discordID: routed.session.userID }) }
+  ]
 
   return routed.res.send({ success: true, data: totals })
 }
