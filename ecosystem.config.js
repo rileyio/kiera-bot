292265b96a4e226e3b68de8c20eec4601a9e9760
Emma RileyIO <riley@tdm.io>
2021-11-06 03:24:57 -0300

    Object keys sorted

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,24 @@
 module.exports = {
   apps: [{
-    name: 'kiera-bot',
-    script: './app/start.js',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    // args: 'one two',
-    // node_args: "DEBUG='kiera-bot:*'",
-    instances: 1,
     autorestart: true,
-    watch: ['app', 'locales'],
-    ignore_watch: ['node_modules', 'logs'],
-    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
     env_production: {
-      NODE_ENV: 'production',
-      DEBUG: 'kiera-bot:*'
+      DEBUG: 'kiera-bot:*',
+      NODE_ENV: 'production'
     },
-    max_restarts: 1
+    ignore_watch: ['node_modules', 'logs'],
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // args: 'one two',
+    // node_args: "DEBUG='kiera-bot:*'",
+    instances: 1,
+    max_memory_restart: '1G',
+    max_restarts: 1,
+    name: 'kiera-bot',
+    script: './app/start.js',
+    watch: ['app', 'locales']
   }],
-
   deploy: {
     production: {
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
