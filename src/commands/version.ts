b9be0197a8280a241d3fe2a6bf891a34e4bf5318
Emma RileyIO <riley@tdm.io>
2021-11-06 03:24:00 -0300

    Updating version cmd type, object key sorting, routed type specified.

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,23 +1,24 @@
+import { ExportRoutes, RoutedInteraction } from '@/router'
+
 import { SlashCommandBuilder } from '@discordjs/builders'
-import { RouterRouted, ExportRoutes } from '@/router'
 
 export const Routes = ExportRoutes({
-  type: 'message',
   category: 'Info',
   controller: versionCheck,
   description: 'Help.Admin.BotVersion.Description',
-  example: '{{prefix}}version',
   name: 'admin-version',
+  permissions: {
+    serverOnly: false
+  },
   slash: new SlashCommandBuilder().setName('version').setDescription('Display current Kiera version'),
-  validate: '/version:string',
-  permissions: { serverOnly: false }
+  type: 'interaction'
 })
 
 /**
  * Gets the bot's current `package.json` version string
  * @export
- * @param {RouterRouted} routed
+ * @param {RoutedInteraction} routed
  */
-function versionCheck(routed: RouterRouted) {
+function versionCheck(routed: RoutedInteraction) {
   return routed.reply(`Running on version \`${routed.bot.version}\``)
 }
