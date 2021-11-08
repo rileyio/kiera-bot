3e2fdfc29feb9f3f673399e9726b67a15b2bf1d0
Emma RileyIO <riley@tdm.io>
2021-11-08 03:39:22 -0400

    Converted Roll command to slash command

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -1,50 +1,44 @@
 import * as Random from 'random'
-import { RouterRouted, ExportRoutes } from '@/router'
-import { rollDie, rollDice } from '@/embedded/roll-embed'
+
+import { ExportRoutes, RoutedInteraction } from '@/router'
+import { rollDice, rollDie } from '@/embedded/roll-embed'
+
+import { SlashCommandBuilder } from '@discordjs/builders'
 
 export const Routes = ExportRoutes({
-  type: 'message',
   category: 'Fun',
   controller: roll,
   description: 'Help.Fun.Roll.Description',
-  example: '{{prefix}}roll',
   name: 'roll-die',
-  validate: '/roll:string/count1?=string/count2?=number',
-  permissions: { serverOnly: false }
+  permissions: {
+    serverOnly: false
+  },
+  slash: new SlashCommandBuilder()
+    .setName('roll')
+    .setDescription('Roll 1 or more dice')
+    .addIntegerOption((option) => option.setName('sides').setDescription('Number of sides for the dice'))
+    .addIntegerOption((option) => option.setName('dice').setDescription('Number of dice to roll')),
+  type: 'interaction'
 })
 
 /**
  * Roll (a die | dice)
  * @export
- * @param {RouterRouted} routed
+ * @param {RoutedInteraction} routed
  */
-export async function roll(routed: RouterRouted) {
-  // Check if the first argument matches the `2d8` spec.
-  if (routed.v.o.count1 || routed.v.o.count1) {
-    if (routed.v.o.count1.match(/^[0-9]+d[0-9]+$/g)) {
-      // Split and assign them to their appropriate variable inputs to simulate a user inputting /roll <amount> <worth>
-      const [amount, worth] = routed.v.o.count1.split('d')
-      routed.v.o.count1 = +amount
-      routed.v.o.count2 = +worth
+async function roll(routed: RoutedInteraction) {
+  const sides = (routed.interaction.options.get('sides')?.value as number) || 6
+  const dice = (routed.interaction.options.get('dice')?.value as number) || 1
+
+  if (dice !== 1) {
+    const set = []
+    for (let index = 0; index < dice; index++) {
+      set.push(Random.int(1, sides))
     }
 
-    if (routed.v.o.count1 && routed.v.o.count2) {
-      const set = []
-      for (let index = 0; index < routed.v.o.count1; index++) {
-        set.push(Random.int(1, routed.v.o.count2))
-      }
-      await routed.message.reply({ embeds: [rollDice(routed.v.o.count2 || 6, routed.v.o.count1, set)] })
-      return true
-    }
-
-    // If there is a number specified as the second argument cast it to a number.
-    if (routed.v.o.count1) {
-      // Perform the conversion of string to num if possible.
-      routed.v.o.count1 = !isNaN(routed.v.o.count1) ? +routed.v.o.count1 : 6
-    }
+    return await routed.reply({ embeds: [rollDice(sides, dice, set)] })
   }
 
-  const set: Array<any> = [Random.int(1, routed.v.o.count1 || 6)]
-  await routed.message.reply({ embeds: [rollDie(Number(routed.v.o.count1 || 6), set)] })
-  return true
+  const set: Array<number> = [Random.int(1, sides)]
+  return await routed.reply({ embeds: [rollDie(Number(sides), set)] })
 }
