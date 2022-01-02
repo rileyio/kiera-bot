762b4edc16c03a5e49af086a46f60cefa8a23140
Emma RileyIO <riley@tdm.io>
2022-01-02 02:18:22 -0400

    Repairing v9 broken imports when running with ts-node importing and adding route loading times and loaded route verbose logging

--- src/router/route-loader.ts
+++ src/router/route-loader.ts
@@ -3,12 +3,18 @@ import * as glob from 'fast-glob'
 
 import { Logger } from '@/utils'
 import { RouteConfiguration } from '@/router'
+import { performance } from 'perf_hooks'
 
 export async function routeLoader(logger: Logger.Debug) {
-  logger.log('loading command routes...')
+  logger.log('loading command routes...', process.env.NODE_ENV ? `mode: ${process.env.NODE_ENV}` : '')
 
   // Load routes from commands folder
-  const _routeFiles = glob.sync(['app/commands/**/*.js', '!app/commands/index.js', '!app/commands/admin.js'], { deep: 5 })
+  const _routeFiles = glob.sync(
+    process.env.NODE_ENV === 'development'
+      ? ['src/commands/**/*.ts', '!src/commands/index.ts', '!src/commands/admin.ts']
+      : ['app/commands/**/*.js', '!app/commands/index.js', '!app/commands/admin.js'],
+    { deep: 3 }
+  )
 
   // Collection of routes
   const routes: Array<RouteConfiguration> = []
@@ -18,6 +24,8 @@ export async function routeLoader(logger: Logger.Debug) {
     const routeFile = _routeFiles[index]
 
     // Wrapped in a try to make more safe when loading and errors are present
+    const start = performance.now()
+    //logger.debug('Trying to load', routeFile.toString())
     try {
       const _requiredFile = (await import(Path.join('../../', routeFile.toString()))) as { Routes: Array<RouteConfiguration> }
       // Test if file returns undefined
@@ -28,13 +36,15 @@ export async function routeLoader(logger: Logger.Debug) {
           const route = _requiredFile.Routes[index]
           routes.push(route)
         }
+
+        logger.verbose(`routeLoader() => route [${routeFile.toString()}] loaded (${Math.round(performance.now() - start)}ms)`)
       }
     } catch (e) {
       logger.error(`routeLoader() [ERROR] => ${routeFile.toString()}, ${e.message}`)
     }
   }
 
-  // console.log(`routeLoader() => routes loaded: ${routes.length}`)
+  logger.verbose(`routeLoader() => routes loaded: ${routes.length}`)
 
   return routes
 }
