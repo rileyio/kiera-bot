1e67438d7631ce07d798d02b03ee3d88392683db
Emma RileyIO <riley@tdm.io>
2022-04-09 10:57:51 -0300

    9.1.1 Fixed: Register command. Will rework this in the future.

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -3,6 +3,7 @@ import * as Utils from '@/utils'
 import { ExportRoutes, RoutedInteraction } from '@/router'
 
 import { SlashCommandBuilder } from '@discordjs/builders'
+import { TrackedUser } from '@/objects/user'
 
 export const Routes = ExportRoutes({
   category: 'User',
@@ -14,16 +15,11 @@ export const Routes = ExportRoutes({
 })
 
 export async function registerUser(routed: RoutedInteraction) {
-  const userSnowflake = routed.user.id
-  const userArgType = Utils.User.verifyUserRefType(userSnowflake)
-  const isRegistered = await routed.bot.DB.verify('users', userSnowflake)
-
-  if (!isRegistered) {
+  if (!(await routed.bot.DB.verify('users', { id: routed.user.id }))) {
     // If not yet registered, store user in db
-    routed.bot.Log.Command.log(`/register ${Utils.User.buildUserChatAt(routed.member, userArgType)}`)
+    await routed.bot.DB.add('users', new TrackedUser({ id: routed.user.id }))
     return await routed.reply(`:white_check_mark: You're now registered! ^_^`, true)
   } else {
-    routed.bot.Log.Command.log(`/register ${Utils.User.buildUserChatAt(userSnowflake, userArgType)} - user already registered`)
-    return await routed.reply(`You're already registered! :wink:`, true)
+    return await routed.reply(`You're already registered! :blush:`, true)
   }
 }
