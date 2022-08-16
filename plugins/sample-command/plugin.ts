effb324aca3345bdd6b0217fcd657218dd7711c6
Emma RileyIO <riley@tdm.io>
2022-08-15 22:27:02 -0300

    sample-command plugin updated for tests

--- plugins/sample-command/plugin.ts
+++ plugins/sample-command/plugin.ts
@@ -6,6 +6,8 @@
  */
 
 import { Plugin } from '../../src/index'
+import { RoutedInteraction } from '../../src/router'
+import { SlashCommandBuilder } from 'discord.js'
 
 export class SampleCommandPlugin extends Plugin {
   config = { testProp: false }
@@ -13,6 +15,30 @@ export class SampleCommandPlugin extends Plugin {
   constructor() {
     super()
     console.log('SampleCommand Plugin Loaded')
+  }
+
+  public async onEnabled() {
+    await this.bot.Router.addRoute({
+      category: 'Plugin',
+      controller: this.routeCommand,
+      name: 'test',
+      permissions: {
+        defaultEnabled: false,
+        serverOnly: false
+      },
+      plugin: this,
+      slash: new SlashCommandBuilder().setName('test').setDescription('Testing Plugin'),
+      type: 'interaction'
+    })
+  }
+
+  public async onDisabled() {
+    await this.bot.Router.removeRoute('test')
+  }
+
+  public async routeCommand(plugin: SampleCommandPlugin, routed: RoutedInteraction) {
+    console.log('routeCommand')
+    return await routed.reply('Should Reply', true)
   }
 }
 
