219ddf385b636ccb3e427d41a56534058767eef2
Emma RileyIO <riley@tdm.io>
2022-10-01 12:57:16 -0300

    Added: Paging buttons to the bottom of the Decision List command output for users with many decision rollers.

--- src/commands/decision/list.cmd.ts
+++ src/commands/decision/list.cmd.ts
@@ -1,13 +1,69 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle } from 'discord.js'
+
 import { RoutedInteraction } from '@/router'
+import { TrackedDecision } from '@/objects/decision'
 import { embed } from '@/commands/decision/list.embed'
 
 export async function list(routed: RoutedInteraction) {
   const authorID = routed.author.id
-  const decisionsStored = await routed.bot.DB.getMultiple('decision', { authorID })
+  const decisionsStored = (await routed.bot.DB.getMultiple('decision', { authorID })) as Array<TrackedDecision>
 
   if (!decisionsStored.length) {
     return await routed.reply('There Are No Decision Rollers to List.')
   }
 
-  return routed.reply({ embeds: [embed(authorID, decisionsStored)] }, true)
+  try {
+    // Display options
+    const outputOptions = new ActionRowBuilder<ButtonBuilder>()
+      .addComponents(new ButtonBuilder().setCustomId('previous').setLabel('Previous').setStyle(ButtonStyle.Secondary).setDisabled(true))
+      .addComponents(new ButtonBuilder().setCustomId('next').setLabel('Next').setStyle(ButtonStyle.Secondary))
+
+    // Display first page if = 0
+    let seek = 0
+
+    // Collector to recieve interaction (With 5m timeout)]
+    console.log('Collector Started!')
+
+    const collector = routed.interaction.channel?.createMessageComponentCollector({ time: 5 * (60 * 1000) })
+    collector?.on('collect', async (i: ButtonInteraction) => {
+      // Disable 'previous' button when going to the first page
+      if (i.customId === 'previous' && seek === 3) outputOptions.components[0].setDisabled(true)
+
+      // Output Button Responses
+      if (i.customId === 'previous' && seek !== 0) {
+        // If the seek coming into this step is not 3, then we can enable the 'previous' button
+        if (seek !== 3) outputOptions.components[0].setDisabled(false)
+        // Enable the 'next' button
+        outputOptions.components[1].setDisabled(false)
+        seek = seek - 3
+        await i.update({ components: [outputOptions], embeds: [embed(authorID, decisionsStored.slice(seek, seek + 3))] })
+      }
+      if (i.customId === 'next') {
+        // Enable the 'previous' button
+        outputOptions.components[0].setDisabled(false)
+        seek = seek + 3
+        // Disable the 'next' button if we are at the end of the list
+        if (seek + 3 >= decisionsStored.length) outputOptions.components[1].setDisabled(true)
+        await i.update({ components: [outputOptions], embeds: [embed(authorID, decisionsStored.slice(seek, seek + 3))] })
+      }
+    })
+
+    collector?.on('end', async (collected, reason) => {
+      if (reason && reason !== 'stopped') {
+        routed.interaction.editReply({
+          components: []
+        })
+      }
+    })
+
+    return routed.reply(
+      decisionsStored.length > 3
+        ? // When more than 3, display options
+          { components: [outputOptions], embeds: [embed(authorID, decisionsStored.slice(seek, seek + 3))] }
+        : { embeds: [embed(authorID, decisionsStored)] },
+      true
+    )
+  } catch (error) {
+    console.error(error)
+  }
 }
