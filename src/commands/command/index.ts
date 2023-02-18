8e1c0c971ce95afea011ddf4686a067314f47d64
Emma (rileyio) <riley@tdm.io>
2023-02-18 02:24:06 -0400

    Well, im still finding stuff to migrate.. Commenting out for now

--- src/commands/command/index.ts
+++ src/commands/command/index.ts
@@ -1,90 +1,91 @@
-import { ExportRoutes, RoutedInteraction } from '@/router'
+// import { ExportRoutes, RoutedInteraction } from '@/router'
 
-import { SlashCommandBuilder } from '@discordjs/builders'
+// import { AcceptedResponse } from '@/objects/router/routed-interaction'
+// import { SlashCommandBuilder } from '@discordjs/builders'
 
-export const Routes = ExportRoutes({
-  category: 'Admin',
-  controller: ckStatsRouterSub,
-  name: 'command',
-  permissions: {
-    defaultEnabled: true,
-    serverAdminOnly: true,
-    serverOnly: true
-  },
-  slash: new SlashCommandBuilder()
-    .setName('command')
-    .setDescription('Manage Commands on this Discord Server')
-    .setDefaultPermission(true) // Settings for an entire Category/Group that commands fall within
-    .addSubcommand(
-      (subcommand) =>
-        subcommand
-          .setName('category')
-          .setDescription('Toggle a whole Category of Commands') // Enable Group of Commands
-          .addStringOption((option) =>
-            option
-              .setName('enable')
-              .setDescription('Enable a Category/Group that multiple command fall within')
-              .addChoices({ name: 'Battle.Net Commands (3rd Party)', value: 'bnet' }, { name: 'Dice, Coins, etc', value: 'fun' })
-          ) // Disable Group of Commands
-          .addStringOption((option) =>
-            option
-              .setName('disable')
-              .setDescription('Disable a Category/Group that multiple command fall within')
-              .addChoices({ name: 'Battle.Net Commands (3rd Party)', value: 'bnet' }, { name: 'Dice, Coins, etc', value: 'fun' })
-          ) // )
-    ),
-  type: 'interaction'
-})
+// export const Routes = ExportRoutes({
+//   category: 'Admin',
+//   controller: ckStatsRouterSub,
+//   name: 'command',
+//   permissions: {
+//     defaultEnabled: true,
+//     serverAdminOnly: true,
+//     serverOnly: true
+//   },
+//   slash: new SlashCommandBuilder()
+//     .setName('command')
+//     .setDescription('Manage Commands on this Discord Server')
+//     .setDefaultPermission(true) // Settings for an entire Category/Group that commands fall within
+//     .addSubcommand(
+//       (subcommand) =>
+//         subcommand
+//           .setName('category')
+//           .setDescription('Toggle a whole Category of Commands') // Enable Group of Commands
+//           .addStringOption((option) =>
+//             option
+//               .setName('enable')
+//               .setDescription('Enable a Category/Group that multiple command fall within')
+//               .addChoices({ name: 'Battle.Net Commands (3rd Party)', value: 'bnet' }, { name: 'Dice, Coins, etc', value: 'fun' })
+//           ) // Disable Group of Commands
+//           .addStringOption((option) =>
+//             option
+//               .setName('disable')
+//               .setDescription('Disable a Category/Group that multiple command fall within')
+//               .addChoices({ name: 'Battle.Net Commands (3rd Party)', value: 'bnet' }, { name: 'Dice, Coins, etc', value: 'fun' })
+//           ) // )
+//     ),
+//   type: 'interaction'
+// })
 
-async function ckStatsRouterSub(routed: RoutedInteraction) {
-  const subCommand = routed.options.getSubcommand()
-  const enable = routed.interaction.options.get('enable')?.value
-  const disable = routed.interaction.options.get('disable')?.value
-  const commands = await routed.guild.commands.fetch()
-  const commandMapped = commands.map((c) => {
-    return {
-      defaultPermission: c.permissions,
-      description: c.description,
-      guildId: c.guildId,
-      id: c.id,
-      name: c.name
-    }
-  })
-  console.log('subCommand:', subCommand, enable || disable)
-  console.log('commands from lookup:', commandMapped)
+// async function ckStatsRouterSub(routed: RoutedInteraction): AcceptedResponse {
+//   const subCommand = routed.options.getSubcommand()
+//   const enable = routed.interaction.options.get('enable')?.value
+//   const disable = routed.interaction.options.get('disable')?.value
+//   const commands = await routed.guild.commands.fetch()
+//   const commandMapped = commands.map((c) => {
+//     return {
+//       defaultPermission: c.permissions,
+//       description: c.description,
+//       guildId: c.guildId,
+//       id: c.id,
+//       name: c.name
+//     }
+//   })
+//   console.log('subCommand:', subCommand, enable || disable)
+//   console.log('commands from lookup:', commandMapped)
 
-  if (subCommand === 'category') {
-    const commandsFromKiera = routed.bot.Router.routes.filter((r) => r.category.toLowerCase() === enable || disable)
-    const permissions = [
-      {
-        id: '224617799434108928',
-        permission: false,
-        type: 'USER'
-      }
-    ]
+//   if (subCommand === 'category') {
+//     const commandsFromKiera = routed.bot.Router.routes.filter((r) => r.category.toLowerCase() === enable || disable)
+//     const permissions = [
+//       {
+//         id: '224617799434108928',
+//         permission: false,
+//         type: 'USER'
+//       }
+//     ]
 
-    console.log('commandsFromKiera:', commandsFromKiera)
-    console.log('permissions:', permissions)
-    // return routed.reply({ content: 'yep.. did a thing' })
-  }
+//     console.log('commandsFromKiera:', commandsFromKiera)
+//     console.log('permissions:', permissions)
+//     // return routed.reply({ content: 'yep.. did a thing' })
+//   }
 
-  console.log('enable:', enable)
-  // console.log('commandsfrom kiera lookup:', commandMapped)
-  // return routed.reply({ content: 'yep.. did a thing' })
-  // const subCommand = routed.interaction.options.getSubcommand()
-  // const interactionType = routed.interaction.options.get('type')?.value
+//   console.log('enable:', enable)
+//   // console.log('commandsfrom kiera lookup:', commandMapped)
+//   // return routed.reply({ content: 'yep.. did a thing' })
+//   // const subCommand = routed.interaction.options.getSubcommand()
+//   // const interactionType = routed.interaction.options.get('type')?.value
 
-  // // Stats
-  // if (subCommand === 'stats') {
-  //   if (interactionType === 'lockee') return CKStats.getLockeeStats(routed)
-  //   if (interactionType === 'lockees') return CKStats.getKeyholderLockees(routed)
-  //   if (interactionType === 'keyholder') return CKStats.getKeyholderStats(routed)
-  //   if (interactionType === 'multilocked') return CKStats.getCheckLockeeMultiLocked(routed)
-  // }
+//   // // Stats
+//   // if (subCommand === 'stats') {
+//   //   if (interactionType === 'lockee') return CKStats.getLockeeStats(routed)
+//   //   if (interactionType === 'lockees') return CKStats.getKeyholderLockees(routed)
+//   //   if (interactionType === 'keyholder') return CKStats.getKeyholderStats(routed)
+//   //   if (interactionType === 'multilocked') return CKStats.getCheckLockeeMultiLocked(routed)
+//   // }
 
-  // // Update
-  // if (subCommand === 'update') return CKUpdate.update(routed)
+//   // // Update
+//   // if (subCommand === 'update') return CKUpdate.update(routed)
 
-  // // Verify
-  // if (subCommand === 'verify') return CKVerify.verify(routed)
-}
+//   // // Verify
+//   // if (subCommand === 'verify') return CKVerify.verify(routed)
+// }
--- src/commands/stats/stats-server-top.cmd.ts
+++ src/commands/stats/stats-server-top.cmd.ts
@@ -1,64 +1,64 @@
-import * as Utils from '@/utils'
+// import * as Utils from '@/utils'
 
-import { ExportRoutes, RouterRouted } from '@/router'
+// import { ExportRoutes, RouterRouted } from '@/router'
 
-import { ServerStatisticType } from '@/objects/statistics'
-import { statsTopServerChannels } from '@/commands/stats/stats-server.embed'
+// import { ServerStatisticType } from '@/objects/statistics'
+// import { statsTopServerChannels } from '@/commands/stats/stats-server.embed'
 
-export const Routes = ExportRoutes({
-  category: 'Stats',
-  controller: statsByTopChannels,
-  description: 'Help.Stats.ViewTopChannelsByStats.Description',
-  example: '{{prefix}}stats top channels',
-  middleware: [],
-  name: 'stats-top-channels',
-  permissions: {
-    defaultEnabled: true,
-    restricted: false,
-    serverOnly: true
-  },
-  type: 'message',
-  validate: '/stats:string/top:string/channels:string'
-})
+// export const Routes = ExportRoutes({
+//   category: 'Stats',
+//   controller: statsByTopChannels,
+//   description: 'Help.Stats.ViewTopChannelsByStats.Description',
+//   example: '{{prefix}}stats top channels',
+//   middleware: [],
+//   name: 'stats-top-channels',
+//   permissions: {
+//     defaultEnabled: true,
+//     restricted: false,
+//     serverOnly: true
+//   },
+//   type: 'message',
+//   validate: '/stats:string/top:string/channels:string'
+// })
 
-export async function statsByTopChannels(routed: RouterRouted) {
-  const data = await routed.bot.DB.aggregate<{ channelID: string; count: number; name?: string }>('stats-servers', [
-    {
-      $match: { serverID: routed.message.guild.id, type: ServerStatisticType.Message }
-    },
-    {
-      $group: {
-        _id: '$channelID',
-        count: { $sum: 1 }
-      }
-    },
-    { $sort: { count: -1 } },
-    {
-      $project: {
-        _id: 0,
-        channelID: '$_id',
-        count: 1
-      }
-    }
-  ])
+// export async function statsByTopChannels(routed: RouterRouted) {
+//   const data = await routed.bot.DB.aggregate<{ channelID: string; count: number; name?: string }>('stats-servers', [
+//     {
+//       $match: { serverID: routed.message.guild.id, type: ServerStatisticType.Message }
+//     },
+//     {
+//       $group: {
+//         _id: '$channelID',
+//         count: { $sum: 1 }
+//       }
+//     },
+//     { $sort: { count: -1 } },
+//     {
+//       $project: {
+//         _id: 0,
+//         channelID: '$_id',
+//         count: 1
+//       }
+//     }
+//   ])
 
-  // Map channel names
-  const mappedData = data
-    .map((stat) => {
-      stat.name = Utils.Channel.buildChannelChatAt(stat.channelID)
-      return stat
-    })
-    // Limit to just the top 20
-    .slice(0, 20)
+//   // Map channel names
+//   const mappedData = data
+//     .map((stat) => {
+//       stat.name = Utils.Channel.buildChannelChatAt(stat.channelID)
+//       return stat
+//     })
+//     // Limit to just the top 20
+//     .slice(0, 20)
 
-  await routed.message.channel.send({
-    embeds: [
-      statsTopServerChannels({
-        data: mappedData,
-        serverIcon: routed.message.guild.iconURL()
-      })
-    ]
-  })
+//   await routed.message.channel.send({
+//     embeds: [
+//       statsTopServerChannels({
+//         data: mappedData,
+//         serverIcon: routed.message.guild.iconURL()
+//       })
+//     ]
+//   })
 
-  return true
-}
+//   return true
+// }
